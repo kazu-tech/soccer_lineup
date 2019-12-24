9e53dd4fcd90e060ebc71ff5486aa8478ee3c724
kazu-tech <o63.ishihara.kazuyuki@gmail.com>
2019-12-24 18:45:31 +0900

    post.newフォーム実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.find(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:date, :match, :result, :formation, :num11, :num10, :num9, :num8, :num7, :num6, :num5, :num4, :num3, :num2, :num1,)
+  end
 end
