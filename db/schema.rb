739b5e9527a8c14ce2f9e1e18bc8a1b51ff5cf70
kazu-tech <o63.ishihara.kazuyuki@gmail.com>
2019-12-25 12:06:31 +0900

    デバイス導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_082753) do
+ActiveRecord::Schema.define(version: 2019_12_24_071700) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date"
+    t.string "match"
+    t.string "result"
+    t.string "formation"
+    t.string "num11"
+    t.string "num10"
+    t.string "num9"
+    t.string "num8"
+    t.string "num7"
+    t.string "num6"
+    t.string "num5"
+    t.string "num4"
+    t.string "num3"
+    t.string "num2"
+    t.string "num1"
+    t.text "text"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: ""
