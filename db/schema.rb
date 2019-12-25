3a0dd3a8b03c5152babcae7caebe86c75d3c4910
kazu-tech <o63.ishihara.kazuyuki@gmail.com>
2019-12-25 12:40:22 +0900

    ユーザーカラム変更

--- db/schema.rb
+++ db/schema.rb
@@ -35,15 +35,12 @@ ActiveRecord::Schema.define(version: 2019_12_24_071700) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: ""
-    t.string "encrypted_password", default: ""
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "name"
+    t.string "email"
+    t.string "encrypted_password"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
