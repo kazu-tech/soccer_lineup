94e14f83c7e4b6ef35846b3047d4bd00a264c11d
kazu-tech <o63.ishihara.kazuyuki@gmail.com>
2019-12-26 17:41:02 +0900

    写真投稿機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email)
+  def user_params   
+    params.require(:user).permit(:name, :email, :image)
   end
 end
-end
+
