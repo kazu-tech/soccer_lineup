bb8c4d36f7b14a5af014dff45adae5c314d4b54f
kazu-tech <o63.ishihara.kazuyuki@gmail.com>
2020-01-07 17:48:39 +0900

    css変更

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,15 @@
-$(function() {
- 
-  //セレクトボックスが切り替わったら発動
-  $('select').change(function() {
- 
-    //選択したvalue値を変数に格納
-    var val = $(this).val();
- 
-    //選択したvalue値をp要素に出力
-    $('p').text(val);
-  });
-});
+function myfunc() {
+  var value　= document.getElementById("myselectbox").value;
+  var target = document.getElementById("target");
+  var text = '';
+  if(value === '東京'){
+    text = '東京にはスカイツリーがあります';
+  }
+  else if(value === '神奈川'){
+    text = '神奈川には鎌倉があります';
+  }
+  else if(value === '千葉'){
+    text = '千葉にはディズニーランドがあります';
+  }
+  target.innerHTML = text;
+}
